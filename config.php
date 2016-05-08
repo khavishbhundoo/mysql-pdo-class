21ed84ef42931d61805a3db27f7d34be6546cc40
khavishbhundoo <khavishbhundoo@users.noreply.github.com>
2016-05-08 22:08:15 +0400

    Added files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Define database configuration
+define("DB_HOST", "localhost");
+define("DB_NAME", "mytest");
+define("DB_CHARSET", "utf8");
+define("DB_USER", "root");
+define("DB_PASS", "weroot"); 
+define("DB_ENGINE", "innodb");
+//Uncomment line below if your MySQL does NOT listen to the standard port(i.e 3306)
+//define("DB_PORT", 3306); 
